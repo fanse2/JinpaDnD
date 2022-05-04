75196c7ba02f4b07b072f7cd87ea3c3b2b89e733
June Choi <fanse2@gmail.com>
2022-05-04 15:56:23 -0700

    ver0.9

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const connal = document.querySelector('#connal')
 
 const masterLine = [
     '때는 30세기 무렵, 진파계는 혼돈에 빠졌다.',
-    '헬가대령은 꼰날 장군의 게릴라 부대를 요격하기 위해 출정 하였다.',
-    '주사위를 굴려서 장기의 말 처럼 이동하여 공략한다.',
+    '헬가대령은 꼰날장군의 게릴라부대를 요격하기 위해 출정.',
+    '드랍쉽이 자기장덫에 잡히고 암호를 모르면 공격을 당하게 된다',
     '포스가 함께하시길~!'
 ]
 
 const connalLine = [
-    '좌표 33,25 지역을 접수하라!',
+    '케찰 지역을 접수하라!',
     '진격하라~!'
 ]
 
 const hellgaLine = [
-    '한명의 해방군도 허용치마라',
+    '드랍쉬의 지원이 오고 있다.',
     '지옥의 빨간맛을 보여주마!'
 ]
 
@@ -61,7 +61,7 @@ async function story() {
     let hellgaPromise = new Promise(resolve => {
         pitchLine(connalLine.shift(),hellga)
         storyInt = setInterval(nextLine,3000,hellgaLine,hellga)
-        setTimeout(function() {resolve("flex");}, 10000);
+        setTimeout(function() {resolve("flex");}, 11000);
     })
 
 
@@ -80,3 +80,108 @@ function nextLine(lines,playerCard) {
 }
 
 playButton.addEventListener('click',story)
+
+let answers = [
+    'tiger',
+    'lion',
+    'crocodile',
+    'dolphin',
+    'elephant',
+    'barracuda',
+    'gorilla',
+    'toad',
+    'coyote',
+    'iguana',
+    'whale',
+    'orangutan',
+    'peacock',
+    'chameleon',
+    'turkey',
+    'cheetah',
+    'armadillo',
+    'bonobo',
+    'camel',
+    'shark'
+]
+
+let answer = ''
+let maxWrong = 6
+let mistakes = 0
+let guessed = []
+let wordStatus = null
+
+
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null
+    document.querySelector('#' + chosenLetter).setAttribute('disabled', true)
+
+
+    if(answer.indexOf(chosenLetter) >= 0){
+        guessedWord()
+        checkIfGameWon()
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++
+        updateMistakes()
+        checkIfGameLost()
+        updateHangmanPicture()
+    }
+}
+
+function randomWord() {
+    answer = answers[Math.floor(Math.random()*answers.length)]
+}
+
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => `
+    <button
+        class='letter-button' id='${letter}' onClick='handleGuess("${letter}")'>
+        ${letter}
+    </button>
+    `).join('')
+    document.querySelector('#keyboard').innerHTML = buttonsHTML
+}
+
+
+function updateHangmanPicture() {
+    document.querySelector('#hangmanPic').src = `./images/SCDropship${mistakes}.jpg`
+}
+
+function checkIfGameWon() {
+    if(wordStatus === answer) {
+        document.querySelector('#wordSpotlight').innerHTML = '드랍쉽이 덫에서 벗어나고 병력투하, 작전성공!'
+        document.querySelector('#keyboard').innerHTML = ''
+    }
+}
+
+
+function checkIfGameLost() {
+    if(mistakes === maxWrong) {
+        document.querySelector('#wordSpotlight').innerHTML = '비밀번호는 ' + answer + ' => 실패'
+        document.querySelector('#keyboard').innerHTML = ''
+    }
+}
+
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('')
+    document.querySelector('#wordSpotlight').innerHTML = wordStatus
+}
+
+function updateMistakes() {
+    document.querySelector('#mistakes').innerHTML = mistakes
+}
+
+function reset(){
+    mistakes = 0
+    guessed = []
+    document.querySelector('#hangmanPic').src = './images/SCDropship0.jpg'
+    randomWord()
+    guessedWord()
+    updateMistakes()
+    generateButtons()
+}
+
+document.querySelector('#maxWrong').innerHTML = maxWrong
+randomWord()
+generateButtons()
+guessedWord()
+story()
